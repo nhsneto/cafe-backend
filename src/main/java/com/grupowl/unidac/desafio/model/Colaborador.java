bffdafb73391d22f03c1c528d9dcd7b6e30595a8
Nelson Henrique <nhsneto@gmail.com>
2023-04-17 17:00:26 -0300

    Adiciona model

---
+++ src/main/java/com/grupowl/unidac/desafio/model/Colaborador.java
@@ -0,0 +1,106 @@
+package com.grupowl.unidac.desafio.model;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+public class Colaborador implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(nullable = false, unique = true, length = 100)
+	private String nome;
+
+	@Column(nullable = false, unique = true, length = 15)
+	private String cpf;
+
+	@OneToMany(mappedBy = "colaborador", cascade = CascadeType.ALL, orphanRemoval = true)
+	@JsonManagedReference
+	private List<Opcao> opcoes;
+
+	@Column(nullable = false, name = "data_cafe")
+	@Temporal(TemporalType.DATE)
+	private LocalDate data;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public List<Opcao> getOpcoes() {
+		return opcoes;
+	}
+
+	public void setOpcoes(List<Opcao> opcoes) {
+		this.opcoes = opcoes;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpf, data, id, nome, opcoes);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Colaborador other = (Colaborador) obj;
+		return Objects.equals(cpf, other.cpf) && Objects.equals(data, other.data) && Objects.equals(id, other.id)
+				&& Objects.equals(nome, other.nome) && Objects.equals(opcoes, other.opcoes);
+	}
+
+	@Override
+	public String toString() {
+		return "Colaborador [id=" + id + ", nome=" + nome + ", cpf=" + cpf + ", opcoes=" + opcoes + ", data=" + data
+				+ "]";
+	}
+}
---
+++ src/main/java/com/grupowl/unidac/desafio/model/Opcao.java
@@ -0,0 +1,93 @@
+package com.grupowl.unidac.desafio.model;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+public class Opcao implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(nullable = false, length = 200)
+	private String nome;
+
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JsonBackReference
+	private Colaborador colaborador;
+
+	@Column(nullable = false, name = "data_cafe")
+	@Temporal(TemporalType.DATE)
+	private LocalDate data;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Colaborador getColaborador() {
+		return colaborador;
+	}
+
+	public void setColaborador(Colaborador colaborador) {
+		this.colaborador = colaborador;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(colaborador, data, id, nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Opcao other = (Opcao) obj;
+		return Objects.equals(colaborador, other.colaborador) && Objects.equals(data, other.data)
+				&& Objects.equals(id, other.id) && Objects.equals(nome, other.nome);
+	}
+
+	@Override
+	public String toString() {
+		return "Opcao [id=" + id + ", nome=" + nome + ", colaborador=" + colaborador + ", data=" + data + "]";
+	}
+}
